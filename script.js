3c1c5439f54edbae5649b982dbc79b53e2f4d921
astha1503 <astha9526@gmail.com>
2025-09-09 19:53:31 +0530

    add front response i have to add functional part

---
+++ script.js
@@ -0,0 +1,27 @@
+const screenText = document.querySelector(".screen-text");
+console.log(screenText);
+const btn = document.querySelectorAll("button");
+const btnArray = Array.from(btn);
+
+
+for(let e = 0; e < btnArray.length; e++){
+    btnArray[e].addEventListener("click", function() {
+        display(this);
+    });
+}
+
+function display(key){
+    let value = key.textContent;
+    if(value == "="){
+        screenText.textContent = ""; // add function which display opersation on pparcicular problem
+    }
+    else if (value == "C"){
+        screenText.textContent = "";
+    }
+    else if (value == ""){
+        screenText.textContent = screenText.textContent.slice(0, -1);
+    }
+    else{
+        screenText.textContent += value;
+    }   
+}
